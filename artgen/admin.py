113f8c18ef7f128e8c4775f2421876b971d69820
admin <tomdev@protonmail.com>
2023-07-12 16:50:27 +0100

    updates

--- artgen/admin.py
+++ artgen/admin.py
@@ -1,7 +1,15 @@
+# admin.py
+
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+
 from .forms import ArticleForm
 
 class ArticleAdmin(admin.AdminSite):
+    site_header = 'Article Admin'
+    site_title = 'Article Admin'
+
     def get_urls(self):
         from django.urls import path
         from .views import create_article_view
@@ -11,5 +19,11 @@ class ArticleAdmin(admin.AdminSite):
             path('create_article/', self.admin_view(create_article_view), name='create_article'),
         ]
         return urlpatterns
+
+    def create_article_link(self):
+        url = reverse('admin:create_article')
+        return format_html('<a href="{}">Create Article</a>', url)
+
+    index_title = create_article_link
 
 admin_site = ArticleAdmin(name='articleadmin')
