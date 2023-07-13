965983369ed28426f9b593de1de5b7386e7b0b47
Tom <tomdev@protonmail.com>
2023-07-13 08:38:00 +0000

    updates

--- artgen/urls.py
+++ artgen/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .admin import admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
 ]
