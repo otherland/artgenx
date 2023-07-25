a6d57492a8702ea5a46c069f1038d64cf190fc37
admin <tomdev@protonmail.com>
2023-07-25 12:21:36 +0100

    updates

--- generate/add_links.py
+++ generate/add_links.py
@@ -5,7 +5,14 @@ import spacy
 from bs4 import BeautifulSoup
 from .serps import get_links
 
-nlp = spacy.load('en_core_web_sm')
+try:
+    nlp = spacy.load('en_core_web_sm')
+except OSError:
+    print('Downloading language model for the spaCy POS tagger\n'
+        "(don't worry, this will only happen once)", file=stderr)
+    from spacy.cli import download
+    download('en_core_web_sm')
+    nlp = spacy.load('en_core_web_sm')
 
 
 def extract_last_four_words(text):
